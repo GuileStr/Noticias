ec185a81c7b6680d5accf3132962775402b39433
Guilsetr <pala.rep@hotmail.com>
2019-06-27 23:53:26 -0500

    Lo metí a un rar porque ni le se al github, jaja

---
+++ src/app/services/noticias.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { RespuestaNoticias } from '../interfaces/interfaces';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NoticiasService {
+
+  apiKey = environment.apiKey;
+  apiUrl = environment.apiURL;
+
+headers = new HttpHeaders({
+'X-Api-key': this.apiKey
+});
+
+  constructor(private http: HttpClient) { }
+
+ private callAPI<T>(query: string) {
+query = this.apiUrl + query;
+
+return this.http.get<T>(query, {headers: this.headers});
+ }
+
+  getNoticias() {
+  return this.callAPI<RespuestaNoticias>('/top-headlines?country=us');
+  }
+
+  getNoticiasByCategory( category: string) {
+  return this.callAPI<RespuestaNoticias>('/top-headlines?country=us&category=${category}');
+  }
+}
